9137ba3b53a871c50e61c279bac7b41f3ba60d80
saanvibharadwaj <28sbharadwaj@castilleja.org>
2026-03-11 19:01:38 -0700

    added cellState back

--- src/Cell.java
+++ src/Cell.java
@@ -5,6 +5,7 @@ public class Cell {
     private final int size;
     private final int row;
     private final int column;
+    private CellState cellState;
 
 
     public Cell(int x, int y, int size, int row, int column){
